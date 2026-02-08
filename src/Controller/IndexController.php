de81814e137bed446479a7eeffd04abe884aeb28
Gordon Lesti <info@gordonlesti.com>
2026-02-08 10:39:49 +0100

    show attempts on dashboard

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -20,19 +20,26 @@ final class IndexController extends AbstractController
         $grades = Fontainebleau::cases();
         $easiestKey = count($grades) - 1;
         $hardestKey = 0;
+        $sendCount = 0;
+        $attemptsCount = 0;
         foreach ($logs as $log) {
-            $gradeKey = array_search($log->getGrade(), $grades);
-            if ($gradeKey < $easiestKey) {
-                $easiestKey = $gradeKey;
+            if ($log->isSuccess()) {
+                $sendCount++;
+                $gradeKey = array_search($log->getGrade(), $grades);
+                if ($gradeKey < $easiestKey) {
+                    $easiestKey = $gradeKey;
+                }
+                if ($gradeKey > $hardestKey) {
+                    $hardestKey = $gradeKey;
+                }
             }
-            if ($gradeKey > $hardestKey) {
-                $hardestKey = $gradeKey;
-            }
+            $attemptsCount += $log->getAttempts();
         }
         $routeCount = count($routeRepo->findAll());
         return $this->render('index.html.twig', [
             'route_count' => $routeCount,
-            'log_count' => count($logs),
+            'attempt_count' => $attemptsCount,
+            'send_count' => $sendCount,
             'easiest' => $routeCount ? $grades[$easiestKey] : null,
             'hardest' => $routeCount ? $grades[$hardestKey] : null,
         ]);
